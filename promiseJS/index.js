08a9a215bf35e41e1f5db101015f078817523e18
meetvasani7016 <meetvasani7016@gmail.com>
2026-06-17 15:40:53 +0530

    add fetchuser function with promise handling

---
+++ promiseJS/index.js
@@ -0,0 +1,26 @@
+const fetchuser = (uid) => {
+    return new Promise((resolve, reject) => {
+        const isconnected = true;
+
+        setTimeout(() => {
+            if (isconnected) {
+                resolve({ userid: uid, uname: "Uday", posts: 24, followers: 512, folloinng: 270, })
+            } else {
+                reject(`ERROR : "user not found"`);
+            }
+        }, 3000);
+    })
+}
+
+fetchuser(110)
+    .then(
+        (output) => {
+            console.log("user found", output)
+        }
+    )
+    .catch(
+        (error) => {
+            console.log(error);
+
+        }
+    )
